97800a74ee1fd63eb006f2b41f3e7ed4b424ee2a
Kevin Schanz <kevinschanz@live.com>
2022-02-08 14:38:08 -0500

    Code style format updates and include logger file name

--- src/bots/reddit/actions/comments/comment_actions.py
+++ src/bots/reddit/actions/comments/comment_actions.py
@@ -12,61 +12,59 @@ from praw.exceptions import APIException
 
 Source = namedtuple('Source', ['name', 'api'])
 
-class Comments():
-  def __init__(self, source='cobe'):
-    self.ready = False
-    self.config = reddit_config.CONFIG
-    self.rapi = reddit_api
-    self.source_name = source
-    self.sources = {
-      "cobe": Source('cobe', Cobe)
-    }
-    self.comments = self.sources.get(self.source_name).api()
 
+class Comments:
+    def __init__(self, source='cobe'):
+        self.ready = False
+        self.config = reddit_config.CONFIG
+        self.rapi = reddit_api
+        self.source_name = source
+        self.sources = {
+            "cobe": Source('cobe', Cobe)
+        }
+        self.comments = self.sources.get(self.source_name).api()
 
-  def init(self):
-    log.info("intiializing comments")
-    self.ready = False
-    self.comments.init()
-    self.ready = True
-    log.info("commenting ready")
+    def init(self):
+        log.info("initializing comments")
+        self.ready = False
+        self.comments.init()
+        self.ready = True
+        log.info("commenting ready")
 
-  def comment(self, roll=1):
-    if not self.ready:
-      log.info("comments need to be initialized")
-      self.init()
+    def comment(self, roll=1):
+        if not self.ready:
+            log.info("comments need to be initialized")
+            self.init()
 
-    if chance(roll):
-      log.info("going to make a comment")
+        if chance(roll):
+            log.info("going to make a comment")
 
-      # keep searching posts until we find one with comments
-      post_with_comments = False
-      while not post_with_comments:
-        # pick a subreddit to comment on
-        subreddit = get_subreddit(getsubclass=True)
-        # get a random hot post from the subreddit
-        post = random.choice(list(subreddit.hot()))
-        # replace the "MoreReplies" with all of the submission replies
-        post.comments.replace_more(limit=0)
+            # keep searching posts until we find one with comments
+            post_with_comments = False
+            while not post_with_comments:
+                # pick a subreddit to comment on
+                subreddit = get_subreddit(getsubclass=True)
+                # get a random hot post from the subreddit
+                post = random.choice(list(subreddit.hot()))
+                # replace the "MoreReplies" with all of the submission replies
+                post.comments.replace_more(limit=0)
 
-        if len(post.comments.list()) > 0:
-          post_with_comments = True
+                if len(post.comments.list()) > 0:
+                    post_with_comments = True
 
-      try:
-        # choose if we're replying to the post or to a comment
-        if chance(self.config.get('reddit_reply_to_comment')):
-          # reply to the post with a response based on the post title
-          log.info('replying directly to post')
-          post.reply(self.comments.get_reply(post.title))
-        else:
-          # get a random comment from the post
-          comment = random.choice(post.comments.list())
-          # reply to the comment
-          log.info('replying to comment')
-          comment.reply(self.comments.get_reply(comment.body))
-      except APIException as e:
-        log.info(f"error commenting: {e}")
-
-
-
-
+            try:
+                # choose if we're replying to the post or to a comment
+                if chance(self.config.get('reddit_reply_to_comment')):
+                    # reply to the post with a response based on the post title
+                    reply_message = self.comments.get_reply(post.title)
+                    log.info(f"replying directly to post with: '{reply_message}'")
+                    post.reply(reply_message)
+                else:
+                    # get a random comment from the post
+                    comment = random.choice(post.comments.list())
+                    # reply to the comment
+                    reply_message = self.comments.get_reply(comment.body)
+                    log.info(f"replying to comment with: '{reply_message}'")
+                    comment.reply(reply_message)
+            except APIException as e:
+                log.info(f"error commenting: {e}")
--- src/logs/logger.py
+++ src/logs/logger.py
@@ -8,13 +8,13 @@ from logs.log_utils import NewLineFileHandler, NewLineStreamHandler
 
 
 file_log_format = logging.Formatter("%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s")
-stream_log_format = logging.Formatter("%(asctime)s %(message)s", "%H:%M:%S")
+stream_log_format = logging.Formatter("%(asctime)s %(filename)s:%(lineno)d - %(message)s", "%H:%M:%S")
 logFile = "info.log"
 
 file_handler = RotatingFileHandler(logFile, mode="a", maxBytes=15 * 1024 * 1024, backupCount=2, encoding=None, delay=0)
 file_handler.setFormatter(file_log_format)
 
-stream_handler =  logging.StreamHandler()
+stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(stream_log_format)
 
 log = logging.getLogger(__name__)
